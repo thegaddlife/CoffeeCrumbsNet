eefa38d5a6fd4d3bbc73fbe0d0f2c9aaf2ab0ac3
BRETT R GADD <brettrgadd@BRETTs-MacBook-Pro.local>
2016-08-06 07:59:56 -0700

    cc browse posts

---
+++ scripts/cc-browse-posts.js
@@ -0,0 +1,11 @@
+var CCBrowsePosts = {
+
+    SetTitle = function() {
+
+        var title = document.getElementsByTagName("h3");
+        var titleText = title.innerText;
+        alert(titleText);
+
+    }
+
+}
