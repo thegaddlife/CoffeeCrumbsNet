5bf244588556545baab32d63e1bb18fbf2e7ab5d
BRETT R GADD <brettrgadd@BRETTs-MacBook-Pro.local>
2016-08-08 08:39:43 -0700

    cc author block

--- scripts/cc-author-block.js
+++ scripts/cc-author-block.js
@@ -1,9 +1,21 @@
 $(function() {
 
     $(document).on("ready", function() {
-        var ccAuthorBlock = $(".author-block:visible");
-        if (ccAuthorBlock.length)
-            alert('author');
+        var authorBlockDiv = $(".author-block:visible");
+        if (authorBlockDiv.length)
+            ccAuthorBlock.InitPlugin(authorBlockDiv);
     });
 
 });
+
+var ccAuthorBlock = {
+
+    InitPlugin: function(block) {
+
+        // find the author id
+
+        // get the author
+
+
+    }
+}
