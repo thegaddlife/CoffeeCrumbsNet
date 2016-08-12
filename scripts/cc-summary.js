bb5ed7cd3cd7d049f3c703accb2044b6b370e50e
BRETT R GADD <brettrgadd@BRETTs-MacBook-Pro.local>
2016-08-11 22:59:18 -0700

    cc author

--- scripts/cc-summary.js
+++ scripts/cc-summary.js
@@ -97,6 +97,7 @@ var ccSummaryBlock2 = {
         // loop over this.LoadedPosts; stop adding summary items
         // for this block if the max has been met
         for (i = 0; i < this.LoadedPosts.length; i++) {
+            debugger;
             if (this.LoadedPosts.length < this.ImagesPerBatch)
                 this.AddSummaryItem(this.LoadedPosts[i]);
         }
