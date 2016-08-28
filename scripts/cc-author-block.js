0e196e36c823b950c5802666e5ca6a8415d10ad1
Brett Gadd <brettgadd@gmail.com>
2016-08-28 15:29:10 -0700

    adding jen batchlor

--- scripts/cc-author-block.js
+++ scripts/cc-author-block.js
@@ -23,7 +23,8 @@ var ccAuthorBlock2 = {
         "55e26f05e4b09e1859652657",
         "551c610ce4b0f86de559f9f5",
         "563ee68ee4b07f78f2d7fd5b",
-        "57c35d3615d5db588da4a20c"
+        "57c35d3615d5db588da4a20c",
+        "57c363a4f5e231b32aacca32"
     ],
 
     SocialLinks: {
